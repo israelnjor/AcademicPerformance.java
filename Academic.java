b2b350fa01bb157ed846d8238197e8a90cd6aaaf
Israelnjor <ibkiche@st.ug.edu.gh>
2026-02-11 07:48:01 +0000

    Refactor Academic class with constructors and methods
    Fix constructor logic, add getter methods, and implement letter grade calculation.

--- Academic.java
+++ Academic.java
@@ -1,1 +1,61 @@
 
+public class Academic {
+  private String studentName;
+  private final int studentId;
+  private double score;
+  private double attendanceRate;
+
+//CONSTRUCTORS
+public Academic(String studentname, int studentId, double score, double attendanceRate) {
+  this.studentName = studentName;
+  this.studentId = studentId;
+  this.score = score;
+  score >= 0 and score <= 100;
+  this.attendanceRate = attendanceRate;
+    if attendanceRate < 0 || attendanceRate > 100:
+      attendancerate = 0;
+
+  //GETTERS
+
+  public String getStudentName() {
+    return studentName;
+  }
+  public int getStudentId() {
+    return studentId;
+  }
+  public double getScore() {
+    return score;
+  }
+  public double getAttendanceRate() {
+    return AttendanceRate;
+  }
+
+  private char calclulateLetterGrade(double score) {
+    if (score <= 100 and score >= 90) {
+      return 'A';
+    }
+    else if (score >= 80) {
+      return 'B';
+    }
+    else if (score >= 70) {
+      return 'C';
+    }
+    else if (score >= 60) {
+      return 'D';
+    }
+    else if (score < 60 and score >= 0) {
+      return F;
+    }
+    else {
+      return "Invalid score";
+    } 
+  }
+
+  public String getAcademicStatus(double score, double attendanceRate) {
+    
+  }
+  
+
+
+  
+  
